6cf4dfb802f0fe9350a5436c2b9909d39ba093c0
VtShabanoff <vitalyshabanoff@yandex.ru>
2021-11-01 20:15:56 +0300

    modify classes RegistrationPersonUseCase, ServerRepository and added enum class CatBreedEnum

--- app/src/main/java/com/vt_shabanoff/petsapp/data/ServerRepository.kt
+++ app/src/main/java/com/vt_shabanoff/petsapp/data/ServerRepository.kt
@@ -1,8 +1,32 @@
 package com.vt_shabanoff.petsapp.data
 
+import com.vt_shabanoff.petsapp.domen.Person
+import com.vt_shabanoff.petsapp.domen.Pet
+
 object ServerRepository {
 
-    fun registrationPerson(){
-
+    fun registrationPerson(): Person {
+        return Person(
+            "nik",
+            "vitaly",
+            "petrov",
+            "male",
+            30,
+            "8988888888",
+            "",
+            "",
+            listOf(
+                Pet(
+                    "Cat",
+                    "222",
+                    "333",
+                    "123",
+                    false,
+                    3,
+                    listOf()
+                )
+            ),
+            1234567890
+        )
     }
 }
