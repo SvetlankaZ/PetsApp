fa2583b3ec8e70e047b02f6ae15b9818385492c9
VtShabanoff <vitalyshabanoff@yandex.ru>
2021-12-22 17:05:11 +0300

    added enum class raccoon

---
+++ app/src/main/java/com/vt_shabanoff/petsapp/domen/enum_classes/RaccoonsBreedEnum.kt
@@ -0,0 +1,18 @@
+package com.vt_shabanoff.petsapp.domen.enum_classes
+
+enum class RaccoonsBreedEnum {
+    /*
+    1. Енот-полоскун
+    2. Енот тресмариасский
+    3. Енот багамский
+    4. Енот косумельский
+    5. Ракоед
+     */
+
+    RACCOON_POLOSUN,
+    THE_RACCOON_OF_TRESMARIAS,
+    THE_BAHAMIAN_RACCOON,
+    COZUMEL_RACCOON,
+    CRUSTACEAN
+
+}
